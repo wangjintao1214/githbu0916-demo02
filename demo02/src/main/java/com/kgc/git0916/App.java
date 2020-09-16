b40c6bff4e9665ff2f4a0df998e6d9f39d9848be
86182 <3271397298@qq.com>
2020-09-16 16:09:11 +0800

    测试！！！！！

---
+++ demo02/src/main/java/com/kgc/git0916/App.java
@@ -0,0 +1,14 @@
+package com.kgc.git0916;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("测试");
+    }
+}
